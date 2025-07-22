2d330c2eb0d31918ee389419c9a7994b03573b79
Dorota Frasunkiewicz <frasunkiewicz.dorota@gmail.com>
2025-07-22 11:26:47 +0200

    Homework 8 part 2: Implementation

--- homework-08/bookstore.js
+++ homework-08/bookstore.js
@@ -22,10 +22,10 @@ class Cart {
         this.books = [];
      
     }
-    addBooks(book){
+    addBook(book){
         this.books.push(book);
     }
-    removeBooks(ISBN) {
+    removeBook(ISBN) {
         this.books = this.books.filter(book => book.ISBN !== ISBN);
     }
     totalPrice() {
@@ -40,3 +40,25 @@ class Order {
         this.total = books.reduce((sum, book) => sum + book.price, 0);
     }
 }
+
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", "12345", 39.99, true);
+const book2 = new Book("1984", "George Orwell", "23456", 29.99, true);
+const book3 = new Book("Clean Code", "Robert C. Martin", "34567", 49.99, true);
+const book4 = new Book("The Pragmatic Programmer", "Andrew Hunt", "45678", 44.99, true);
+
+const user1 = new User("Dorota", "dorota@example.com", 1);
+const user2 = new User("Bob", "bob@example.com", 2);
+
+const cart1 = new Cart(user1);
+cart1.addBook(book1);
+cart1.addBook(book2);
+
+const cart2 = new Cart(user2);
+cart2.addBook(book3);
+cart2.addBook(book4);
+
+const order1 = new Order(cart1.user, cart1.books);
+const order2 = new Order(cart2.user, cart2.books);
+
+console.log(order1);
+console.log(order2);
