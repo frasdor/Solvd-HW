fe9d3c0365d1eebcae2c6ebb4f0060f5017cdb6d
Dorota Frasunkiewicz <frasunkiewicz.dorota@gmail.com>
2025-08-14 15:00:50 +0200

    Homework 10: Part 2.2: Collision Handling, Part 3: Building a Hash Table - 1. Hash Table Class

--- homework-10/task1.js
+++ homework-10/task1.js
@@ -11,3 +11,62 @@ console.log(customHash("cat", tableSize));
 console.log(customHash("dog", tableSize));   
 console.log(customHash("car", tableSize));   
 console.log(customHash("bike", tableSize));  
+
+// Custom hash function
+class CustomHashTable {
+    constructor(size = 10) {
+        this.table = new Array(size);
+        this.size = size;
+    }
+
+    insert(key, value) {
+        const index = customHash(key, this.size);
+
+        if (!this.table[index]){
+            this.table[index] = [];
+        }
+
+        for (let i = 0; i < this.table[index].length; i++) {
+            if (this.table[index][i][0] === key) {
+                this.table[index][i][1] = value;
+                return;
+            }
+        }
+
+        // If it doesn't exist — add a new pair
+        this.table[index].push([key, value]);
+    }
+
+    // Get value by key
+  get(key) {
+    const index = customHash(key, this.size);
+    const bucket = this.table[index];
+
+    if (!bucket) return undefined;
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        return bucket[i][1];
+      }
+    }
+
+    return undefined;
+  }
+
+  // Delete a key-value pair
+  delete(key) {
+    const index = customHash(key, this.size);
+    const bucket = this.table[index];
+
+    if (!bucket) return false;
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket.splice(i, 1);
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
